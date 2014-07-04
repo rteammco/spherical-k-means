b3a879a711defb7b2f1d175d3d70a7bba29f615f
Richard Teammco <teammco@cs.utexas.edu>
2014-07-03 20:08:26 -0500

    Moved ClusterData to a class with its own .cpp file.

---
+++ CPP/src/cluster_data.cpp
@@ -0,0 +1,173 @@
+/* File: cluster_data.cpp
+ *
+ * Defines the ClusterData functions used to assist the SPKMeans class.
+ * This class keeps track of the algorithm's data structures, and provdes
+ * basic data manipulation and memory management methods.
+ */
+
+#include "cluster_data.h"
+
+
+
+// Constructor: pass in the three required values (k, wc, dc), and setup
+// the appropriate data structures.
+// If pointers to the lists are not provided, new lists will be
+// initialized instead.
+ClusterData::ClusterData(int k_, int dc_, int wc_,
+    float **concepts_, int *p_asgns_, float *doc_priorities_,
+    bool *changed_, float *cValues_, float *qualities_)
+{
+    // set the variables (k, document count, word count)
+    k = k_;
+    dc = dc_;
+    wc = wc_;
+
+    // set partitions pointer
+    partitions = new float**[k];
+
+    // set partition sizes pointer
+    p_sizes = new int[k];
+
+    // set concepts pointer
+    if(concepts_== 0)
+        concepts = new float*[k];
+    else
+        concepts = concepts_;
+
+    // set partition assignment pointers
+    if(p_asgns_ == 0)
+        p_asgns = new int[dc];
+    else
+        p_asgns = p_asgns_;
+    p_asgns_new = new int[dc];
+
+    // set document priorities pointer
+    if(doc_priorities_ == 0)
+        doc_priorities = new float[dc];
+    else
+        doc_priorities = doc_priorities_;
+
+    // set changed flag pointer; if newly created, initialize all to true
+    if(changed_ == 0) {
+        changed = new bool[k];
+        for(int i=0; i<k; i++)
+            changed[i] = true;
+    }
+    else
+        changed = changed_;
+
+    // set cosine similarity cache pointer
+    if(cValues_ == 0)
+        cValues = new float[k*dc];
+    else
+        cValues = cValues_;
+
+    // set partition qualities cache pointer
+    if(qualities_ == 0)
+        qualities = new float[k];
+    else
+        qualities = qualities_;
+}
+
+
+
+// Destructor: calls its own clean up function
+ClusterData::~ClusterData()
+{
+    clearMemory();
+}
+
+
+
+// Gives document doc a new partition assignment part (these are indices).
+// These partitions are stored in the p_asgns_new array, and NOT the
+// default one. Use swapAssignments to update.
+// This function also automatically updates the partition change values.
+void ClusterData::assignPartition(int doc, int partition)
+{
+    p_asgns_new[doc] = partition;
+    if(p_asgns[doc] != partition) {
+        changed[partition] = true;
+        changed[p_asgns[doc]] = true;
+    }
+}
+
+
+
+// Assigns a partition to this document (same as assignPartition above),
+// but also updates the document's priority.
+void ClusterData::assignPartition(int doc, int partition, float priority)
+{
+    assignPartition(doc, partition);
+    doc_priorities[doc] = priority;
+}
+
+
+
+// Swaps the p_assignments and new_p_assignments pointers such so that
+// the new values are updated without needing to manipulate memory.
+void ClusterData::swapAssignments()
+{
+    int *temp = p_asgns;
+    p_asgns = p_asgns_new;
+    p_asgns_new = temp;
+}
+
+
+
+// Cleans up partition array by deleting the list of document vectors in
+// each partition slot. The vectors themselves are NOT deleted, nor is
+// the actual partition list.
+void ClusterData::clearPartitions()
+{
+    for(int i=0; i<k; i++)
+        delete[] partitions[i];
+}
+
+
+
+// Cleans concept vector memory, permanently deleting all of the concept
+// vectors. The actual concept vector list is NOT deleted.
+void ClusterData::clearConcepts()
+{
+    for(int i=0; i<k; i++)
+        delete[] concepts[i];
+}
+
+
+
+// Cleans up all structs that can be cleaned, and frees up all used memory.
+// WARNING: this will turn all data structure pointers to NULL.
+void ClusterData::clearMemory()
+{
+    // clean up partition, size, and concept vector arrays
+    if(partitions != 0) {
+        clearPartitions();
+        delete[] partitions;
+        partitions = 0;
+    }
+    if(p_sizes != 0) {
+        delete[] p_sizes;
+        p_sizes = 0;
+    }
+    if(concepts != 0) {
+        clearConcepts();
+        delete[] concepts;
+        concepts = 0;
+    }
+
+    // clean up change cache arrays
+    if(changed != 0) {
+        delete[] changed;
+        changed = 0;
+    }
+    if(cValues != 0) {
+        delete[] cValues;
+        cValues = 0;
+    }
+    if(qualities != 0) {
+        delete[] qualities;
+        qualities = 0;
+    }
+}
+
--- CPP/src/cluster_data.h
+++ CPP/src/cluster_data.h
@@ -1,24 +1,26 @@
 /* File: cluster_data.h
  *
- * Contains a struct for the clustering algorithm(s) to use to easily store
- * and return the partitions, partition sizes, and concept vectors, along
- * with other variables.
+ * Contains a class for the clustering algorithm(s) to use and easily store
+ * and return the partition assignments, caching arrays, concept vectors,
+ * and other data structures used by spherical k-means.
  */
 
 #ifndef CLUSTER_DATA_H
 #define CLUSTER_DATA_H
 
 
-// ClusterData struct can contain partition and concept vector pointers, and
+// ClusterData class can contain partition and concept vector pointers, and
 // functions to clean out the memory.
-struct ClusterData
-{
+class ClusterData {
+
+  public:
+
     // clustering variables (k, word count, document count)
     int k;
     int dc;
     int wc;
 
-    // pointers to partitions (OLD CODE)
+    // pointers to partitions (OLD CODE) TODO - remove
     float ***partitions;
     int *p_sizes;
 
@@ -35,159 +37,31 @@ struct ClusterData
     float *qualities;
 
 
-    // Constructor: pass in the three required values (k, wc, dc), and set
-    // partition, concept vector, and change caching pointers optionally.
-    // If pointers to the lists are not provided, new lists will be
-    // initialized instead.
+    // Constructor: sets up variables and data structures.
     ClusterData(int k_, int dc_, int wc_,
             float **cvs_ = 0, int *p_asgns_ = 0, float *doc_priorities_ = 0,
-            bool *changed_ = 0, float *cValues_ = 0, float *qualities_ = 0)
-    {
-        // set the variables (k, document count, word count)
-        k = k_;
-        dc = dc_;
-        wc = wc_;
+            bool *changed_ = 0, float *cValues_ = 0, float *qualities_ = 0);
 
-        // set partitions pointer
-        partitions = new float**[k];
+    // Destructor: calls its own clean up function.
+    ~ClusterData();
 
-        // set partition sizes pointer
-        p_sizes = new int[k];
+    // Assigns a partition to the given document.
+    void assignPartition(int doc, int partition);
 
-        // set concepts pointer
-        if(cvs_ == 0)
-            concepts = new float*[k];
-        else
-            concepts = cvs_;
+    // Assigns a partition and priority to the given document.
+    void assignPartition(int doc, int partition, float priority);
 
-        // set partition assignment pointers
-        if(p_asgns_ == 0)
-            p_asgns = new int[dc];
-        else
-            p_asgns = p_asgns_;
-        p_asgns_new = new int[dc];
+    // Swaps new assignments for the default ones (updates the assignments).
+    void swapAssignments();
 
-        // set document priorities pointer
-        if(doc_priorities_ == 0)
-            doc_priorities = new float[dc];
-        else
-            doc_priorities = doc_priorities_;
+    // Cleans up partition array. TODO - remove
+    void clearPartitions();
 
-        // set changed flag pointer; if newly created, initialize all to true
-        if(changed_ == 0) {
-            changed = new bool[k];
-            for(int i=0; i<k; i++)
-                changed[i] = true;
-        }
-        else
-            changed = changed_;
+    // Cleans concept vector memory.
+    void clearConcepts();
 
-        // set cosine similarity cache pointer
-        if(cValues_ == 0)
-            cValues = new float[k*dc];
-        else
-            cValues = cValues_;
-
-        // set partition qualities cache pointer
-        if(qualities_ == 0)
-            qualities = new float[k];
-        else
-            qualities = qualities_;
-    }
-
-
-    // Destructor: calls its own clean up function
-    ~ClusterData()
-    {
-        clearMemory();
-    }
-
-
-    // Gives document doc a new partition assignment part (these are indices).
-    // These partitions are stored in the p_asgns_new array, and NOT the
-    // default one. Use swapAssignments to update.
-    // This function also automatically updates the partition change values.
-    void assignPartition(int doc, int partition)
-    {
-        p_asgns_new[doc] = partition;
-        if(p_asgns[doc] != partition) {
-            changed[partition] = true;
-            changed[p_asgns[doc]] = true;
-        }
-    }
-
-
-    // Assigns a partition to this document (same as assignPartition above),
-    // but also updates the document's priority.
-    void assignPartition(int doc, int partition, float priority)
-    {
-        assignPartition(doc, partition);
-        doc_priorities[doc] = priority;
-    }
-
-
-    // Swaps the p_assignments and new_p_assignments pointers such so that
-    // the new values are updated without needing to manipulate memory.
-    void swapAssignments()
-    {
-        int *temp = p_asgns;
-        p_asgns = p_asgns_new;
-        p_asgns_new = temp;
-    }
-
-
-    // Cleans up partition array by deleting the list of document vectors in
-    // each partition slot. The vectors themselves are NOT deleted, nor is
-    // the actual partition list.
-    void clearPartitions()
-    {
-        for(int i=0; i<k; i++)
-            delete[] partitions[i];
-    }
-
-
-    // Cleans concept vector memory, permanently deleting all of the concept
-    // vectors. The actual concept vector list is NOT deleted.
-    void clearConcepts()
-    {
-        for(int i=0; i<k; i++)
-            delete[] concepts[i];
-    }
-
-
-    // clean up the partitions and concept vector pointers
-    void clearMemory()
-    {
-        // clean up partition, size, and concept vector arrays
-        if(partitions != 0) {
-            clearPartitions();
-            delete[] partitions;
-            partitions = 0;
-        }
-        if(p_sizes != 0) {
-            delete[] p_sizes;
-            p_sizes = 0;
-        }
-        if(concepts != 0) {
-            clearConcepts();
-            delete[] concepts;
-            concepts = 0;
-        }
-
-        // clean up change cache arrays
-        if(changed != 0) {
-            delete[] changed;
-            changed = 0;
-        }
-        if(cValues != 0) {
-            delete[] cValues;
-            cValues = 0;
-        }
-        if(qualities != 0) {
-            delete[] qualities;
-            qualities = 0;
-        }
-    }
+    // Clean up all data memory.
+    void clearMemory();
 
 };
 
