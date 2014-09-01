47dc150d65a6f228b615aa6dd4cdea336c273925
Richard Teammco <teammco@cs.utexas.edu>
2014-09-01 15:22:26 -0500

    Updated pure Galois code to read .gr binary files. Added mat.gr to the TestData directory.

--- Galois/src/spkmeans_galois.cpp
+++ Galois/src/spkmeans_galois.cpp
@@ -6,9 +6,13 @@
  */
 
 #include <iostream>
+#include <string>
 
 #include "Galois/Galois.h"
 #include "Galois/Graph/Graph.h"
+//#include "Galois/LCGraph.h"
+#include "Galois/Timer.h"
+//#include "Galois/Statistic.h"
 
 #include "boost/iterator/counting_iterator.hpp"
 
@@ -16,10 +20,48 @@ using namespace std;
 
 
 
+/*********************** GALOIS STRUCTS AND DEFINITIONS ***********************/
+
+
+// graph node defined here (for both documents and features)
+struct Node {
+    float *x;
+};
+
+
+// define this graph type as just "Graph"
+typedef Galois::Graph::LC_CSR_Graph<Node, float> Graph;
+
+
+/******************************************************************************/
+
+
+
 // Main (start here):
 // Reads arguments and graph file, and sets up Galois.
-int main()
+int main(int argc, char ** argv)
 {
+    // process arguments parameters
+    if (argc < 3) {
+        cout << "Usage: <.gr data file> <k> <threads>" << endl;
+        return 0;
+    }
+    const char *file = argv[1];
+    const int k = atoi(argv[2]);
+    int n_threads = atoi(argv[3]);
+
+    // initialize Galois threads
+    Galois::setActiveThreads(n_threads);
+    n_threads = Galois::getActiveThreads();
+    string s = "s";
+    if (n_threads == 1)
+        s = "";
+    cout << "Running with " << n_threads << " thread" << s << "." << endl;
+
+    // read graph file
+    Graph g;
+    Galois::Graph::readGraph(g, file);
+
     cout << "It works!" << endl;
     return 0;
 }
