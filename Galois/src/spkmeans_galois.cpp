6497a44d695fe414839b9f3ed4850d3ded752e78
Richard Teammco <teammco@cs.utexas.edu>
2014-09-01 15:53:05 -0500

    Galois: graph initialization code done. Word count and document count are now available.

--- Galois/src/spkmeans_galois.cpp
+++ Galois/src/spkmeans_galois.cpp
@@ -23,17 +23,54 @@ using namespace std;
 /*********************** GALOIS STRUCTS AND DEFINITIONS ***********************/
 
 
-// graph node defined here (for both documents and features)
+// Graph node data defined here (for both documents and features).
 struct Node {
-    float *x;
+    // TODO - fill this out appropriately
+    unsigned int cluster_id;
 };
 
 
-// define this graph type as just "Graph"
+// Define this graph type as just "Graph" for convenience.
 typedef Galois::Graph::LC_CSR_Graph<Node, float> Graph;
 
 
+// The core processing struct used by Galois to run SPKMeans.
+struct PartitionBasic {
+    ;
+};
+
+
 /******************************************************************************/
+
+
+
+// Initializes the graph data to prepare it for clustering.
+// Returns the number of document nodes in the graph.
+unsigned int init(Graph &g)
+{
+    unsigned int max_id = 0;
+
+    for (Graph::iterator it = g.begin(), end = g.end(); it != end; it++) {
+        Graph::GraphNode node = *it;
+        Node &data = g.getData(node);
+
+        unsigned int num_edges =
+            g.edge_end(node, Galois::NONE) - g.edge_begin(node, Galois::NONE);
+
+        // if this node has edges, then it is a document node, so set max_id
+        unsigned int cur_id = (unsigned int) node;
+        if (num_edges > 0 && max_id < cur_id)
+            max_id = cur_id;
+
+        // if document, initialize its node data
+        if (num_edges > 0) {
+            // TODO - fill this out appropriately
+            data.cluster_id = 0;
+        }
+    }
+
+    return max_id;
+}
 
 
 
@@ -47,8 +84,8 @@ int main(int argc, char ** argv)
         return 0;
     }
     const char *file = argv[1];
-    const int k = atoi(argv[2]);
-    int n_threads = atoi(argv[3]);
+    const unsigned int k = atoi(argv[2]);
+    unsigned int n_threads = atoi(argv[3]);
 
     // initialize Galois threads
     Galois::setActiveThreads(n_threads);
@@ -62,6 +99,10 @@ int main(int argc, char ** argv)
     Graph g;
     Galois::Graph::readGraph(g, file);
 
-    cout << "It works!" << endl;
+    // initialize the graph data
+    unsigned int dc = init(g);
+    unsigned int wc = g.size() - dc;
+    cout << "Data: " << dc << " documents and " << wc << " words." << endl;
+
     return 0;
 }
