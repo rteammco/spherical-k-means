c7cf609f0cee92a5c609e4af7df5cd05504f2cac
Richard Teammco <teammco@cs.utexas.edu>
2014-09-01 10:59:51 -0500

    Set up Boost and Galois includes in Galois version.

--- Galois/src/spkmeans_galois.cpp
+++ Galois/src/spkmeans_galois.cpp
@@ -1,8 +1,23 @@
+/* File: spkmeans_galois.cpp
+ *
+ * A pure Galois-only implementation of spherical k-means. This is a simpler
+ * non-object-oriented version that reads data directly from Galois graph
+ * files. It is a "prototype" implementation.
+ */
+
 #include <iostream>
+
+#include "Galois/Galois.h"
+#include "Galois/Graph/Graph.h"
+
+#include "boost/iterator/counting_iterator.hpp"
 
 using namespace std;
 
 
+
+// Main (start here):
+// Reads arguments and graph file, and sets up Galois.
 int main()
 {
     cout << "It works!" << endl;
